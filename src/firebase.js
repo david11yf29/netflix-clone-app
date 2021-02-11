23486945a5f5f637f985000604d6f8777488eb17
david11yf29 <david11yf29@gmail.com>
2021-02-11 09:57:07 +0800

    updated

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIOjKMhqMwZOpWrp_xzFEIR0g86Uklq3A",
+  authDomain: "netflix-clone-app-b22fe.firebaseapp.com",
+  projectId: "netflix-clone-app-b22fe",
+  storageBucket: "netflix-clone-app-b22fe.appspot.com",
+  messagingSenderId: "1048125490272",
+  appId: "1:1048125490272:web:c192502fc90a133239af05",
+  measurementId: "G-RTBTS9W3H0",
+};
