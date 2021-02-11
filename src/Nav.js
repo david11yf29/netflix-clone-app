d4199186918211b32e50d81d7ad5cb86ed598170
david11yf29 <david11yf29@gmail.com>
2021-02-11 13:02:56 +0800

    updated

--- src/Nav.js
+++ src/Nav.js
@@ -3,8 +3,19 @@ import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div className="nav">
-      <h1>This is the nav</h1>
+    <div className="nav nav__black">
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
