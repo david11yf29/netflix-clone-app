674dd2b49b26ad277e2bc56fbdea9bd31e83688f
david11yf29 <david11yf29@gmail.com>
2021-02-13 00:36:25 +0800

    updated

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -9,6 +9,11 @@ const SignupScreen = () => {
         <input placeholder="Email" type="email" />
         <input placeholder="Password" type="password" />
         <button type="submit">Sign In</button>
+
+        <h4>
+          <span className="signupScreen__gray">New to Netflix? </span>
+          Sign Up now.
+        </h4>
       </form>
     </div>
   );
