6fd439eb233f6c5af39fa688279656b674422781
david11yf29 <david11yf29@gmail.com>
2021-02-12 17:37:59 +0800

    updated

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -10,6 +10,7 @@ const Login = () => {
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
+        <button className="loginScreen__button">Sign In</button>
       </div>
     </div>
   );
