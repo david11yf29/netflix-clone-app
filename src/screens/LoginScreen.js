0ab741f5fd20bdb2fa5f1cd3b0e56322400d80ee
david11yf29 <david11yf29@gmail.com>
2021-02-12 19:45:15 +0800

    updated

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -11,6 +11,8 @@ const Login = () => {
           alt=""
         />
         <button className="loginScreen__button">Sign In</button>
+
+        <div className="loginScreen__gradient" />
       </div>
     </div>
   );
