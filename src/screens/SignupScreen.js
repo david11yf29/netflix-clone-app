2635f579672daf9b9b6c35b225ddc9855ef4f4e1
david11yf29 <david11yf29@gmail.com>
2021-02-13 00:07:48 +0800

    updated

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./SignupScreen.css";
 
 const SignupScreen = () => {
-  return <div className="signupScreen"></div>;
+  return (
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignupScreen;
