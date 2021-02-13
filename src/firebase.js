2faeea61f45e22598fa78d471effd3e392216f9b
david11yf29 <david11yf29@gmail.com>
2021-02-14 00:59:41 +0800

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIOjKMhqMwZOpWrp_xzFEIR0g86Uklq3A",
-  authDomain: "netflix-clone-app-b22fe.firebaseapp.com",
-  projectId: "netflix-clone-app-b22fe",
-  storageBucket: "netflix-clone-app-b22fe.appspot.com",
-  messagingSenderId: "1048125490272",
-  appId: "1:1048125490272:web:c192502fc90a133239af05",
-  measurementId: "G-RTBTS9W3H0",
+  apiKey: "AIzaSyD1B1CJOen_wpU7RdsSkBBVmPxJfQvb_V0",
+  authDomain: "netflix-clone-yt-4bdfa.firebaseapp.com",
+  projectId: "netflix-clone-yt-4bdfa",
+  storageBucket: "netflix-clone-yt-4bdfa.appspot.com",
+  messagingSenderId: "889054173176",
+  appId: "1:889054173176:web:2f2cb536aca9c5145ea80d",
+  measurementId: "G-ZE9FTQ8QHH",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
