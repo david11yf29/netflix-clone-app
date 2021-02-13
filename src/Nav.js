54622a96c3f3be7502c7723809aa08e1cb7e7d3d
david11yf29 <david11yf29@gmail.com>
2021-02-13 17:25:53 +0800

    updated

--- src/Nav.js
+++ src/Nav.js
@@ -23,6 +23,7 @@ const Nav = () => {
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
         <img
+          onClick={() => history.push("/")}
           className="nav__logo"
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
