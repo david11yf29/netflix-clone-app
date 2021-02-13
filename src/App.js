60f98af43a128e332d5f8ec775805d0c2cad7594
david11yf29 <david11yf29@gmail.com>
2021-02-13 15:56:00 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+
+import { auth } from "./firebase";
 
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth);
+      } else {
+        // Logged Out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
       <Router>
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -26,6 +26,18 @@ const SignupScreen = () => {
 
   const signIn = (e) => {
     e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
