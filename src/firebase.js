ed88c527ceaf513b7430d7004eb9285cb57203ea
david11yf29 <david11yf29@gmail.com>
2021-02-13 10:45:49 +0800

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBIOjKMhqMwZOpWrp_xzFEIR0g86Uklq3A",
   authDomain: "netflix-clone-app-b22fe.firebaseapp.com",
@@ -8,3 +9,10 @@ const firebaseConfig = {
   appId: "1:1048125490272:web:c192502fc90a133239af05",
   measurementId: "G-RTBTS9W3H0",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
