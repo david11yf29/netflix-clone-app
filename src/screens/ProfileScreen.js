3904e1cd03600b2802b0acd41f11ca0c58503122
david11yf29 <david11yf29@gmail.com>
2021-02-13 17:24:47 +0800

    updated

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Nav from "../Nav";
 import "./ProfileScreen.css";
 
 const ProfileScreen = () => {
   return (
     <div className="profileScreen">
-      <h1>This is the profile</h1>
+      <Nav />
     </div>
   );
 };
