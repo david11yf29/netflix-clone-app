208bf20512b89a95ba11ae86f0979646d89e670f
david11yf29 <david11yf29@gmail.com>
2021-02-14 23:21:30 +0800

    updated

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PlansScreen.css";
 
+import db from "../firebase";
+
 const PlansScreen = () => {
-  return <div className="plansScreen"></div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    db.collection("products")
+      .where("active", "==", true)
+      .get()
+      .then((querySnapshot) => {
+        const products = {};
+        querySnapshot.forEach(async (productDoc) => {
+          products[productDoc.id] = productDoc.data();
+          const priceSnap = await productDoc.ref.collection("prices").get();
+          priceSnap.docs.forEach((price) => {
+            products[productDoc.id].prices = {
+              priceId: price.id,
+              priceData: price.data(),
+            };
+          });
+        });
+        setProducts(products);
+      });
+  }, []);
+
+  console.log(products);
+
+  return (
+    <div className="plansScreen">
+      {Object.entries(products).map(([productId, productData]) => {
+        return (
+          <div className="plansScreen__plan">
+            <div className="plansScreen__info">
+              <h5>{productData.name}</h5>
+              <h6>{productData.description}</h6>
+            </div>
+
+            <button>Subscribe</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PlansScreen;
